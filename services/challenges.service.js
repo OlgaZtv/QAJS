0db679ca50bedb2ef3f5820844135a3b08da73dc
OlgaZtv <zatulivetrova@gmail.com>
2022-07-14 16:51:37 +0300

    added yml

--- services/challenges.service.js
+++ services/challenges.service.js
@@ -1,6 +1,8 @@
 import { allure } from 'allure-mocha/dist/MochaAllureReporter';
 import supertest from 'supertest';
 import urls from '../config/urls';
+import { loadApispec, validate } from '../lib/validator';
+
 
 const Chanllenges = {
     get: async(token)=>{
@@ -9,6 +11,10 @@ const Chanllenges = {
         .set('Accept', 'application/json')
         .set('X-CHALLENGER', token);
         allure.attachment('response', JSON.stringify(response.body), 'application/json');
+
+        const apiSpec = await loadApispec('./lib/Simple-Todo-List-swagger.json');
+        const schema = apiSpec.paths['/challenges'].get.responses[200];
+        validate(schema, response.body);
         return response;
     }
 }; 
